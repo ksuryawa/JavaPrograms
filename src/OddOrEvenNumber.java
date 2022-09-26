78836e06be10fdd0954ce43ff9bda3c8a1e7160e
ksuryawa <kapil.r.suryawanshi@gmail.com>
2022-09-26 21:29:19 +0100

    Add program find number is odd or even without % operator in java

---
+++ src/OddOrEvenNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * @author Kapil Suryawanshi
+ * 26/09/2022
+ */
+public class OddOrEvenNumber {
+	public static void main(String[] args) {
+		int num= 134;
+
+		if((num&1)==0)
+		{
+			System.out.println(" Number is Even");
+		}
+		else
+		{
+			System.out.println(" Number is Odd");
+		}
+	}
+}
